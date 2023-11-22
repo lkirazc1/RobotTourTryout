b1c63e7cc10d7b9b2389a721c6cbb194e1128fe0
Brainstorm8 <147956284+Brainstorm8@users.noreply.github.com>
2023-11-21 20:30:30 -0800

    Add files via upload

---
+++ Documents/PlatformIO/Projects/RobotTour2/src/mapping.cpp
@@ -0,0 +1,53 @@
+class Point {
+    public: //constructor
+        int xCord;
+        int yCord;
+        Point(int x, int y) {
+            xCord = x;
+            yCord = y;
+        }
+
+        int getX() {
+            return xCord;
+        }
+
+        int getY() {
+            return yCord;
+        }
+};
+
+// Point point1 = Point(0,0);
+Point list[2] = {Point(0,1), Point(1,1)} ;
+
+void Path(Point cords[], Point pInitial, int initial_Direction) { //note that the input points must draw ONLY vertical or horizontal lines
+    for(int i = 0; i < sizeof(cords); i++) { //goes through all the list of cords
+        int deltaX = cords[i].getX() - pInitial.getX();
+        int deltaY = cords[i].getY() - pInitial.getY();
+
+        int final_Direction;
+        if(deltaX > 0) { //right
+            final_Direction = 90;
+        } else if(deltaX < 0) { //left
+            final_Direction = 270;
+        } else if(deltaY > 0) { //up
+            final_Direction = 0;
+        } else if(deltaY < 0) { //down
+            final_Direction = 180;
+        } else {
+            final_Direction = initial_Direction;
+        }
+
+        //turn in final direction, move
+    }
+}
+
+//take in a list of points
+//take in initial point
+//take in initial direction
+
+//calculate how many right turns it takes to move in the right direction
+
+//move a certain amount of times forward
+
+//set current direction and point as initial point and direction
+
